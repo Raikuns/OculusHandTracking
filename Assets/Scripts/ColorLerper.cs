a4a909aee2255f0f22ebb87dffaa11047bb1fdb8
Joop Westra <joopwestra@gmail.com>
2021-02-01 10:05:26 +0100

    Initial commit

---
+++ Assets/Scripts/ColorLerper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLerper : MonoBehaviour
+{
+    public SkinnedMeshRenderer smr;
+
+    public Color color;
+
+    public string applyToShaderParam;
+    public string readFromShaderParam;
+
+    Color newColor;
+
+    public void UpdateAlpha(float lerp)
+    {
+        newColor = color;
+        newColor.a = lerp;
+        smr.material.SetColor(applyToShaderParam, newColor);
+    }
+
+    public void SetColor(Color color)
+    {
+        this.color = color;
+    }
+
+    public void SetColorFromMaterial(Material material)
+    {
+        this.color = material.GetColor(readFromShaderParam);
+    }
+}
